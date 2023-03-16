5be6f0b62741afca2e659fb9308a1885c1fb6bb6
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-16 21:52:27 +0100

    project getting more interesting

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -7,16 +7,30 @@ const timeout = function(s) {
         }, s * 1000);
     });
 };
-console.log("hi");
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+        const res = await fetch(// `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+        `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc990`);
         const data = await res.json();
-        console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            title: recipe.title,
+            servings: recipe.servings,
+            ingredients: recipe.ingredients,
+            cookingTime: recipe.cooking_time
+        };
+        console.log(recipe);
+    // console.log(res, data);
     } catch (error) {
         console.log(error);
+        alert(error);
     }
 };
 showRecipe();
