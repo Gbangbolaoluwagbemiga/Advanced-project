4d86e0dea50077f7c2d4838c608b54b98d63ed33
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-16 18:22:15 +0100

    project commencement

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +10,19 @@ const timeout = function (s) {
   });
 };
 
+console.log('hi');
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+    console.log(res, data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+showRecipe();
