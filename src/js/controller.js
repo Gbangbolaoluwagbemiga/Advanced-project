2ed06a3e16bbb96abcf3dca76380911def0eb4c7
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-17 23:04:35 +0100

    logging out for the day: over!

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,7 +6,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-/*
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
@@ -14,7 +13,7 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
-*/
+timeout(5);
 
 // https://forkify-api.herokuapp.com/v2
 
